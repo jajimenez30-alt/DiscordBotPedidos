c00a023c991a6f6a8fac9b3a556d2a742f6ecaee
jajimenez30-alt <jajimenez30@gmail.com>
2025-11-13 18:15:42 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-
-# Exportar la variable PORT requerida por Render para evitar el timeout
-# Esto engaña a Render haciéndole creer que hay un puerto abierto
-export PORT=10000 
-
-# Iniciar tu bot de Python
 python bot.py
